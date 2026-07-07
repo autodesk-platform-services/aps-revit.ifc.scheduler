72e9b16afd3cecdf3d50ce42ee1c4cef9a7d0293
Eason Kang <eason.kang@autodesk.com>
2026-07-07 19:44:28 +0800

    feat: add PostgreSQLRevitIfcContext for design-time PostgreSQL migration generation

---
+++ Context/PostgreSQLRevitIfcContext.cs
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Developer Advocacy and Support
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using Microsoft.EntityFrameworkCore;
+
+namespace RevitToIfcScheduler.Context
+{
+    // Design-time-only subclass used by `dotnet ef migrations add` to generate
+    // the PostgreSQL migration set under Migrations/PostgreSQL/. This class is
+    // NOT registered in DI; the runtime always uses RevitIfcContext with the
+    // provider chosen by DatabaseProviderConfiguration in Startup.cs.
+    public class PostgreSQLRevitIfcContext : RevitIfcContext
+    {
+        public PostgreSQLRevitIfcContext(DbContextOptions<RevitIfcContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(
+                    "Host=localhost;Database=RevitIFCScheduler;Username=postgres;Password=postgres",
+                    b => b.MigrationsAssembly("RevitToIfcScheduler"));
+            }
+        }
+    }
+}
